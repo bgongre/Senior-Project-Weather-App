e86aa0fd6a7b87b05bf84a64a7806f575ed3897c
akamath27 <90484551+akamath27@users.noreply.github.com>
2021-09-25 20:27:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -218,6 +218,10 @@ function clearfore(){
 
 }
 
+//Once settings have been updated give users a notication "Abhay Kamath"
+function update(){
+   alert("Your settings have been updated.");
+}
 
 
 
