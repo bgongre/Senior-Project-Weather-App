ece8135ef26b373df646ac45050c5fb5416b3778
akamath27 <90484551+akamath27@users.noreply.github.com>
2021-11-23 20:06:58 -0500

    Update premium.js

--- public/premium.js
+++ public/premium.js
@@ -1,51 +1,71 @@
-var config = {
-  //firebase API Key  "Xiaodong Huang"
-    apiKey: "AIzaSyDbDPVmyCGWItkyTIsDSS9hvYvUrSIPG8Y",
-    authDomain: "senior-project-weather-a-2f0e4.firebaseapp.com",
-    projectId: "senior-project-weather-a-2f0e4",
-    storageBucket: "senior-project-weather-a-2f0e4.appspot.com",
-    messagingSenderId: "619155365766",
-    appId: "1:619155365766:web:d1f57996d24196cc521607"
-  };
+  // Import the functions you need from the SDKs you need
 
-    firebase.initializeApp(config);
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.1/firebase-app.js";
+  import { getDatabase, ref, set, child, update, remove} from "https://www.gstatic.com/firebasejs/9.4.1/firebase-database.js";
 
-function signUpMonthly(){
+
+
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  /* Your web app's Firebase configuration
+    const firebaseConfig = {
+      apiKey: "AIzaSyDbDPVmyCGWItkyTIsDSS9hvYvUrSIPG8Y",
+      authDomain: "senior-project-weather-a-2f0e4.firebaseapp.com",
+  databaseURL: "https://senior-project-weather-a-2f0e4-default-rtdb.firebaseio.com/",
+      projectId: "senior-project-weather-a-2f0e4",
+      storageBucket: "senior-project-weather-a-2f0e4.appspot.com",
+      messagingSenderId: "619155365766",
+      appId: "1:619155365766:web:d1f57996d24196cc521607"
+    };
+
+  // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const db = getDatabase();*/
+
+
 var billAddress = document.getElementById("billAddress").value;
- var fname = document.getElementById("FirstName").value;
- var lname = document.getElementById("LastName").value;
- var cardNum = document.getElementById("CardNum").value;
- var cardDate = document.getElementById("CardDate").value;
+ var fname = document.getElementById("firstName").value;
+ var lname = document.getElementById("lastName").value;
+ var cardNum = document.getElementById("cardNumber").value;
+ var cardDate = document.getElementById("cardDate").value;
  var cvv = document.getElementById("CVV").value;
 
- database.ref('users/').set({
-   billingAdd: billAddress,
-   firstName: fname,
-   lastName: lname,
-   cardNamber: cardNum,
-   cardDate: cardDate,
-   cvv: cvv
+ let uid = firebase.auth().currentUser.uid
+ console.log(uid);
+
+
+ document.getElementById("completePaymentMonthly").addEventListener("click",  function premiumSignUp() {
+      set(ref(db, "users/" + uid), {
+   billingAdd: billAddress.value,
+   firstName: fname.value,
+   lastName: lname.value,
+   cardNamber: cardNum.value,
+   cardDate: cardDate.value,
+   cvv: cvv.value,
  })
+     .then(()=>{
+        alert("Payment Complete")
+     })
+     .catch((error) =>{
+        alert("fail " + error)
+     });
+   })
 
-alert("Payment complete");
-}
 
-function signUpAnnually(){
-  var billAddress = document.getElementById("billAddress").value;
-  var fname = document.getElementById("FirstName").value;
-  var lname = document.getElementById("LastName").value;
-  var cardNum = document.getElementById("CardNum").value;
-  var cardDate = document.getElementById("CardDate").value;
-  var cvv = document.getElementById("CVV").value;
-
-  database.ref('users/').set({
-    billingAdd: billAddress,
-    firstName: fname,
-    lastName: lname,
-    cardNamber: cardNum,
-    cardDate: cardDate,
-    cvv: cvv
-  })
-
-  alert("Payment complete");
-}
+   document.getElementById("completePaymentAnnually").addEventListener("click",  function premiumSignUp() {
+         set(ref(db, "users/" + uid), {
+      billingAdd: billAddress.value,
+      firstName: fname.value,
+      lastName: lname.value,
+      cardNamber: cardNum.value,
+      cardDate: cardDate.value,
+      cvv: cvv.value,
+    })
+        .then(()=>{
+           alert("Payment Complete")
+        })
+        .catch((error) =>{
+           alert("fail " + error)
+        });
+      })
