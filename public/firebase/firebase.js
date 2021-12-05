788e50d16ffe9f4f5e638fcf81f2f254f9bf4340
LemonDrop <brittani.gongre@gmail.com>
2021-12-05 01:19:31 -0500

    added comments to code

--- public/firebase/firebase.js
+++ public/firebase/firebase.js
@@ -2,6 +2,7 @@ var fireBase = fireBase || firebase;
 var hasInit = false;
 var config = {
   //firebase API Key  "Xiaodong Huang"
+  // Added databaseURL information -Brittani Gongre
     apiKey: "AIzaSyDbDPVmyCGWItkyTIsDSS9hvYvUrSIPG8Y",
     authDomain: "senior-project-weather-a-2f0e4.firebaseapp.com",
     databaseURL: "https://senior-project-weather-a-2f0e4-default-rtdb.firebaseio.com/",
@@ -10,6 +11,8 @@ var config = {
     messagingSenderId: "619155365766",
     appId: "1:619155365766:web:d1f57996d24196cc521607"
   };
+  
+  // Added database reference -Brittani Gongre
 if(!hasInit){
     firebase.initializeApp(config);
     database = firebase.database();
